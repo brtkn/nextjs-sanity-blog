0d104b6a7948141f1762e9cfc329df626848d6ab
brtkn <haha>
2023-07-24 21:25:40 +0300

    blog name chagned

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center w-full">
           <Link href="/">
             <h1 className="text-2xl font-medium">
-              Jan <span className="text-teal-500 ">Blog</span>
+              Brt <span className="text-teal-500 ">Blog</span>
             </h1>
           </Link>
           <ThemeButton />
